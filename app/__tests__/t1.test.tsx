2bf3123bfbb68df80cffbe486e5b4c7d4b0f8c13
Hy Nguyen <khanghy2130@gmail.com>
2023-10-13 19:26:06 -0500

    fixed jest pathing

--- app/__tests__/t1.test.tsx
+++ app/__tests__/t1.test.tsx
@@ -3,13 +3,13 @@ import userEvent from '@testing-library/user-event'
 import "@testing-library/jest-dom"
 
 
-import Index from '~/routes/_index';
+import Index from 'app/routes/_index';
 
 test('adds 1 + 2 to equal 3', () => {
     expect(1+2).toBe(3);
 });
 
-test('loads and displays greeting', async () => {
+test('clicking to change text', async () => {
     // ARRANGE
     render(<Index/>)
   
--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
   },
+  moduleDirectories: ['node_modules', '<rootDir>'],
   testPathIgnorePatterns: ["node_modules"],
   transformIgnorePatterns: ['/node_modules\/(?!my-package)(.*)'],
   testEnvironment: "jest-environment-jsdom",
