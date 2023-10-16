85357d8b40714ff7ccd2c6a2c75284156c740ac0
Hy Nguyen <khanghy2130@gmail.com>
2023-10-16 02:04:56 -0500

    attempt to fix github actions error

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,8 @@ module.exports = {
     "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
   },
   moduleDirectories: ['node_modules', '<rootDir>'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+
   testPathIgnorePatterns: ["node_modules"],
   transformIgnorePatterns: ['/node_modules\/(?!my-package)(.*)'],
   testEnvironment: "jest-environment-jsdom",
