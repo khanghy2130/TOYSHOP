d33f53f8aa608ddd4aee01b5ef7fb09633a66730
Hy Nguyen <khanghy2130@gmail.com>
2024-03-18 12:27:29 -0500

    renamed profile route

---
+++ app/routes/myprofile.tsx
@@ -0,0 +1,7 @@
+export default function MyProfile() {
+    return (
+        <div>
+            <h1>My Profile (no one else can see my profile)</h1>
+        </div>
+    );
+}
--- app/routes/user.$id.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "@remix-run/react"
-
-export default function Index() {
-    const {id} = useParams();
-
-    return (<div>
-        <h1>Username: {id}</h1>
-    </div>)
-}
