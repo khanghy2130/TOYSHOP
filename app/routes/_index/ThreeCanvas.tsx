e425b17323d7c73a82448dddf5f7abd45315f998
Hy Nguyen <khanghy2130@gmail.com>
2024-09-30 03:27:04 -0500

    add picture frame

--- app/routes/_index/ThreeCanvas.tsx
+++ app/routes/_index/ThreeCanvas.tsx
@@ -1,38 +1,119 @@
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
-import { TextureLoader, Mesh } from "three";
+import { TextureLoader, Mesh, Group } from "three";
+import * as THREE from "three";
 
 import img1 from "~/assets/landing_page_products_images/img1.jpg";
 import img2 from "~/assets/landing_page_products_images/img2.jpg";
 import img3 from "~/assets/landing_page_products_images/img3.jpg";
 import img4 from "~/assets/landing_page_products_images/img4.jpg";
 
+type PictureFrameProps = {
+    imageSize: [number, number, number];
+    frameThickness: number;
+    frameDepth: number;
+    frameColor: string;
+};
+
+function PictureFrame({
+    imageSize,
+    frameThickness,
+    frameDepth,
+    frameColor,
+}: PictureFrameProps) {
+    const frameWidth = imageSize[0] + frameThickness;
+    const frameHeight = imageSize[1] + frameThickness;
+    const frameMaterial = new THREE.MeshStandardMaterial({
+        color: frameColor,
+        metalness: 2,
+        roughness: 0,
+    });
+
+    return (
+        <>
+            {/* Top frame piece */}
+            <mesh position={[0, imageSize[1] / 2 + frameThickness / 2, 0]}>
+                <boxGeometry
+                    args={[
+                        frameWidth + frameThickness,
+                        frameThickness,
+                        frameDepth,
+                    ]}
+                />
+                <primitive object={frameMaterial} attach="material" />
+            </mesh>
+
+            {/* Bottom frame piece */}
+            <mesh position={[0, -imageSize[1] / 2 - frameThickness / 2, 0]}>
+                <boxGeometry
+                    args={[
+                        frameWidth + frameThickness,
+                        frameThickness,
+                        frameDepth,
+                    ]}
+                />
+                <primitive object={frameMaterial} attach="material" />
+            </mesh>
+
+            {/* Left frame piece */}
+            <mesh position={[-imageSize[0] / 2 - frameThickness / 2, 0, 0]}>
+                <boxGeometry args={[frameThickness, frameHeight, frameDepth]} />
+                <primitive object={frameMaterial} attach="material" />
+            </mesh>
+
+            {/* Right frame piece */}
+            <mesh position={[imageSize[0] / 2 + frameThickness / 2, 0, 0]}>
+                <boxGeometry args={[frameThickness, frameHeight, frameDepth]} />
+                <primitive object={frameMaterial} attach="material" />
+            </mesh>
+        </>
+    );
+}
+
 function FlippingSquare() {
+    const imgSize: [number, number, number] = [3, 3, 0.1];
     const images = [img1, img2, img3, img4];
-    const meshRef = useRef<Mesh>(null);
-    const [index, setIndex] = useState(0);
-    const texture = useLoader(TextureLoader, images[index]);
+    const [imgIndex, setImgIndex] = useState<number>(0);
+    const imgMeshRef = useRef<Mesh>(null);
+    const frameMeshRef = useRef<Group>(null);
+    const texture = useLoader(TextureLoader, images[imgIndex]);
+    const { viewport } = useThree();
+    let scaling = Math.min(1, viewport.width / 5);
 
     let rotation = Math.PI / 2;
 
     useFrame(() => {
-        if (!meshRef.current) return;
+        if (!imgMeshRef.current || !frameMeshRef.current) return;
         // Rotate the square frame
         rotation += 0.02;
-        meshRef.current.rotation.y = rotation;
+        imgMeshRef.current.rotation.y = rotation;
+        frameMeshRef.current.rotation.y = rotation;
 
         // Change the image when the frame completes a flip (180 degrees)
         if (Math.abs(rotation % ((Math.PI / 2) * 3)) < 0.05) {
-            setIndex((index + 1) % images.length); // Loop through the images
+            setImgIndex((imgIndex + 1) % images.length); // Loop through the images
         }
     });
 
     return (
-        <mesh ref={meshRef}>
-            <boxGeometry args={[3, 3, 0.01]} />
-            <meshBasicMaterial map={texture} />
-        </mesh>
+        <>
+            {/* image mesh */}
+            <mesh ref={imgMeshRef} scale={scaling}>
+                <boxGeometry args={imgSize} />
+                <meshBasicMaterial map={texture} />
+            </mesh>
+
+            {/* frame mesh */}
+            <group ref={frameMeshRef} scale={scaling}>
+                <PictureFrame
+                    imageSize={imgSize}
+                    frameThickness={0.2}
+                    frameDepth={0.4}
+                    frameColor={"#fc36b0"}
+                />
+            </group>
+        </>
     );
 }
 
@@ -51,7 +132,12 @@ export default function ThreeCanvas() {
             className="left-0 top-0 -z-10 h-full w-full"
             style={{ position: "fixed" }}
         >
-            <ambientLight />
+            <ambientLight intensity={0.1} />
+            <directionalLight
+                color={"#ffffff"}
+                position={[0, 10, 10]}
+                intensity={1.5}
+            />
             <FlippingSquare />
         </Canvas>,
         document.body,
