2b555827ae6a4e5fc5927e7875cb7e949963d61d
Hy Nguyen <khanghy2130@gmail.com>
2024-10-02 03:28:47 -0500

    scroll animation

--- app/routes/_index/ThreeCanvas.tsx
+++ app/routes/_index/ThreeCanvas.tsx
@@ -8,9 +8,13 @@ import img1 from "~/assets/landing_page_products_images/img1.jpg";
 import img2 from "~/assets/landing_page_products_images/img2.jpg";
 import img3 from "~/assets/landing_page_products_images/img3.jpg";
 import img4 from "~/assets/landing_page_products_images/img4.jpg";
+import img5 from "~/assets/landing_page_products_images/img5.jpg";
+import img6 from "~/assets/landing_page_products_images/img6.jpg";
+import img7 from "~/assets/landing_page_products_images/img7.jpg";
+import img8 from "~/assets/landing_page_products_images/img8.jpg";
 
 type PictureFrameProps = {
-    imageSize: [number, number, number];
+    imageSize: [number, number];
     frameThickness: number;
     frameDepth: number;
     frameColor: string;
@@ -27,7 +31,6 @@ function PictureFrame({
     const frameMaterial = new THREE.MeshStandardMaterial({
         color: frameColor,
         metalness: 2,
-        roughness: 0,
     });
 
     return (
@@ -72,45 +75,69 @@ function PictureFrame({
 }
 
 function FlippingSquare() {
-    const imgSize: [number, number, number] = [3, 3, 0.1];
-    const images = [img1, img2, img3, img4];
-    const [imgIndex, setImgIndex] = useState<number>(0);
     const imgMeshRef = useRef<Mesh>(null);
     const frameMeshRef = useRef<Group>(null);
-    const texture = useLoader(TextureLoader, images[imgIndex]);
     const { viewport } = useThree();
-    let scaling = Math.min(1, viewport.width / 5);
 
-    let rotation = Math.PI / 2;
+    const [scrollY, setScrollY] = useState(0);
+    const imgSize: [number, number] = [2, 2];
+    const images = [img1, img2, img3, img4, img5, img6, img7, img8];
 
-    useFrame(() => {
-        if (!imgMeshRef.current || !frameMeshRef.current) return;
-        // Rotate the square frame
-        rotation += 0.02;
-        imgMeshRef.current.rotation.y = rotation;
-        frameMeshRef.current.rotation.y = rotation;
+    // Update scrollY on scroll
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
 
-        // Change the image when the frame completes a flip (180 degrees)
-        if (Math.abs(rotation % ((Math.PI / 2) * 3)) < 0.05) {
-            setImgIndex((imgIndex + 1) % images.length); // Loop through the images
-        }
-    });
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const scaling = Math.min(1, viewport.width / 3);
+    const deg90 = Math.PI / 2;
+    const rotationYValue = scrollY * 0.005 + deg90;
+    const imgIndex =
+        Math.floor((rotationYValue - deg90) / (deg90 * 2)) % images.length;
+    const texture = useLoader(TextureLoader, images[Math.max(imgIndex, 0)]);
+
+    const calculatedRotation: [number, number, number] = [
+        0.1,
+        rotationYValue,
+        -1.3 + scrollY * 0.002,
+    ];
+    const calculatedPosition: [number, number, number] = [
+        Math.cos(scrollY * 0.004) * 0.35 * scaling,
+        Math.min(-10 + scrollY * 0.03, 0),
+        Math.max(15 - scrollY * 0.05, 0),
+    ];
 
     return (
         <>
             {/* image mesh */}
-            <mesh ref={imgMeshRef} scale={scaling}>
-                <boxGeometry args={imgSize} />
-                <meshBasicMaterial map={texture} />
+            <mesh
+                ref={imgMeshRef}
+                scale={scaling}
+                rotation={calculatedRotation}
+                position={calculatedPosition}
+            >
+                <planeGeometry args={imgSize} />
+                <meshStandardMaterial map={texture} side={THREE.DoubleSide} />
             </mesh>
 
             {/* frame mesh */}
-            <group ref={frameMeshRef} scale={scaling}>
+            <group
+                ref={frameMeshRef}
+                scale={scaling}
+                rotation={calculatedRotation}
+                position={calculatedPosition}
+            >
                 <PictureFrame
                     imageSize={imgSize}
-                    frameThickness={0.2}
-                    frameDepth={0.4}
-                    frameColor={"#fc36b0"}
+                    frameThickness={0.08}
+                    frameDepth={0.2}
+                    frameColor={"#595959"}
                 />
             </group>
         </>
@@ -131,12 +158,13 @@ export default function ThreeCanvas() {
         <Canvas
             className="left-0 top-0 -z-10 h-full w-full"
             style={{ position: "fixed" }}
+            camera={{ fov: 40, position: [0, 0, -5] }}
         >
-            <ambientLight intensity={0.1} />
+            <ambientLight intensity={1.2} />
             <directionalLight
-                color={"#ffffff"}
-                position={[0, 10, 10]}
-                intensity={1.5}
+                color={"#fff"}
+                position={[3, 5, -10]}
+                intensity={1.8}
             />
             <FlippingSquare />
         </Canvas>,
