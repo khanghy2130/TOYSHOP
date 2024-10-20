5ba9d2075e6aa08385126c17ed00f30fd7d24bc2
Hy Nguyen <khanghy2130@gmail.com>
2024-10-20 14:23:56 -0500

    landing page: moved BriefCard header down

--- app/routes/_index/BriefCard.tsx
+++ app/routes/_index/BriefCard.tsx
@@ -14,7 +14,7 @@ export default function BriefCard({ highlight }: Props) {
     return (
         <div ref={briefRef} className="flex w-full p-1 sm:w-1/2 sm:p-2">
             <div className="flex flex-col px-0 py-4 sm:px-4">
-                <h1 className="-mb-2 ms-4 text-4xl font-bold text-primaryColor sm:text-5xl">
+                <h1 className="-mb-3 ms-4 text-4xl font-bold text-primaryColor sm:text-5xl">
                     {highlight.headline}
                 </h1>
                 <p className="w-full flex-grow rounded-lg bg-primaryColor px-3 py-5 text-2xl text-primaryTextColor shadow-md sm:rounded-xl sm:text-3xl">
